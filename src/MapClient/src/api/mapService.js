2a7f8b5e71ccb78ffe0c84772cd840a075062425
MkEger <145021810+MkEger@users.noreply.github.com>
2026-05-14 07:25:05 +0300

    Hardcode API URL in mapService

--- src/MapClient/src/api/mapService.js
+++ src/MapClient/src/api/mapService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = "http://32.192.198.95:8080/api/v1/client";
 
 // Fetch the center coordinates for the initial map load
 export const fetchCenterCoordinates = async (apiUrl) => {
